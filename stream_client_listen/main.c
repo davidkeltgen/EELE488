6c90c4a96069ffc5165269e031ab081b92df64a1
davidkeltgen <dkeltgen45@gmail.com>
2015-01-08 15:37:35 -0700

    Initiated project files for socket programming
    created stream_server_send and stream_client_listen projects

---
+++ stream_client_listen/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Team Ross
+ *
+ * Created on January 8, 2015, 3:35 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
