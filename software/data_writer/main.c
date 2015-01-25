a999a3bc880c679f0149dcfb88c5233b272ac59c
david <david@dave>
2015-01-25 13:11:18 -0700

    modified data_writer to reflect the change in the file format. Now writes CSVs. I also changed the directory structure to linux, with the resonon directory at the source

--- software/data_writer/main.c
+++ software/data_writer/main.c
@@ -11,11 +11,12 @@
 /*
  *  Simple program that is creating data files for the means, standard deviations, and classification parameters
  *  Will receive this information from resonon, but for now, the data being generated is just a counting pattern.
+ *  and will be written as CSVs. 
  */
 int main(int argc, char** argv) {
-    char * MEANS_FILEPATH = "D:/David/c_code/EELE488/input_handler/resonon/input/means/mean.txt";
-    char * STANDARDS_FILEPATH = "D:/David/c_code/EELE488/input_handler/resonon/input/standards/standard.txt";
-    char * CLASS_FILEPATH = "D:/David/c_code/EELE488/input_handler/resonon/input/class/class.txt";
+    char * MEANS_FILEPATH = "/resonon/input/means/mean.csv";
+    char * STANDARDS_FILEPATH = "/resonon/input/standards/standard.csv";
+    char * CLASS_FILEPATH = "/resonon/input/class/class.csv";
     FILE *outfile; // for writing
     int means_size = 1088;
     int standards_size = 1088;
@@ -29,6 +30,15 @@ int main(int argc, char** argv) {
     for( i=0; i < class_size; i++)
     {
         class_matrix[i] = (int *) malloc(num_classes * sizeof(int));
+    }
+    int counter = 0;
+    for(i = 0; i < class_size; i++)
+    {
+        for( j = 0; j < num_classes; j++)
+        {
+            class_matrix[i][j] = counter;
+            counter++;
+        }
     }
     
     int means[means_size];
@@ -49,36 +59,81 @@ int main(int argc, char** argv) {
     outfile = fopen(MEANS_FILEPATH, "a"); // write to this file
     for(i = 0; i < means_size; i++)
     {
-        fwrite(&means[i], sizeof(int), 1, outfile);
+        char msg[100];
+        strstr(msg,"hello");
+        if(i == standards_size -1)
+        {
+            sprintf(msg, "%d", means[i]);
+            //fwrite(&standards[i], sizeof(int), 1, outfile);
+            
+        }
+        else
+        {
+            sprintf(msg, "%d,",means[i]);
+        }
+        printf("flag1\n");
+        printf(msg);
+        fwrite(msg, 1, strlen(msg), outfile);
     }
     fclose(outfile);
+    printf("flag2\n");
     
     /* Write the Standard Deviations to a text file, should be ~4.3KB*/
     outfile = fopen(STANDARDS_FILEPATH, "a"); // write to this file
-    for(i = 0; i < means_size; i++)
+    for(i = 0; i < standards_size; i++)
     {
-        fwrite(&standards[i], sizeof(int), 1, outfile);
+        char msg[100];
+        if(i == standards_size -1)
+        {
+            sprintf(msg, "%d", standards[i]);
+            //fwrite(&standards[i], sizeof(int), 1, outfile);
+            
+        }
+        else
+        {
+            sprintf(msg, "%d,",standards[i]);
+        }
+        fwrite(msg, sizeof (msg[0]), strlen(msg), outfile);
+        
     }
     fclose(outfile);
     
     /* Write the Classification Coefficients to a text file, should be ~217KB*/
-    int counter = 0;
+    //int counter = 0;
     int temp_counter;
-    
+    printf("flag1\n");
     outfile = fopen(CLASS_FILEPATH, "a"); // write to this file
-    for(i = 0; i < class_size; i++)
+    for(i = 0; i < class_size; i++) //class size is the number of columns
     {
         for(j = 0; j < num_classes; j++)
         {
-            temp_counter = counter;
+            
+                    char msg[100];
+        if(i == (class_size* num_classes) -1)
+        {
+            sprintf(msg, "%d", class_matrix[i][j]);
+            
+        }
+        else
+        {
+            sprintf(msg, "%d,",class_matrix[i][j]);
+        }
+                    
+                    
+        fwrite(msg,1,strlen(msg), outfile);
+        printf(msg);    
+            
+            
+            
+            //temp_counter = counter;
             //temp_counter = temp_counter >> 3;
-            printf("%4x", temp_counter);
-            fwrite(&temp_counter, sizeof(int), 1, outfile);
-            counter++;
+            //printf("%4x", temp_counter);
+            //fwrite(&temp_counter, sizeof(int), 1, outfile);
+            //counter++;
         }
     }
     fclose(outfile);
-    
+    printf("flag2\n");
     
     
     
