d12535503de5522887e33d5423f587c72abf0827
davidkeltgen <dkeltgen45@gmail.com>
2015-02-13 15:03:22 -0700

    Added fixed point program and transfer ui

---
+++ software/FileTransferUI/main.c
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.c
+ * Author: david
+ *
+ * Created on January 24, 2015, 12:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int val = 1;
+    
+    if(val == 1)
+        /*Run linux file transfer program*/
+        //scp /home/stacy/images/image*.jpg stacy@myhost.com:/home/stacy/archive
+        //system("scp from to");
+    else
+        /*Run windows file transfer program*/
+    return (EXIT_SUCCESS);
+}
+
