93e381cdceb8bc5c48864b894b5ec84f7c2c7bff
davidkeltgen <dkeltgen45@gmail.com>
2015-03-17 19:49:05 -0600

    cleaning up code, still havent tested it yet

---
+++ software/hps/hps_application/means.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+ * Author: David Keltgen                                                    *
+ * Company: Montana State University                                        *
+ * File Name: means.h                                                       *
+ * Date:  January 3 2015                                                    *
+ * Description: *  
+ *				*
+ *				*
+ *				*
+ ****************************************************************************/
+
+#ifndef MEANS_H
+#define	MEANS_H
+
+/*
+*********************************************************************************************************
+*                                                 INCLUDE FILES
+*********************************************************************************************************
+*/ 
+
+#include "system.h"
+
+/*
+*********************************************************************************************************
+*                                                 DEFINES
+*********************************************************************************************************
+*/ 
+#define  MEANS_PATH        	 	"/resonon/input/means/means.csv"
+#define  MEANS_BASE_ADDRESS
+#define  MEANS_MATRIX_SIZE		1088   /*Could be read in from header file */
+
+/* global vars */
+
+/* 2D classification matrix*/
+double * means_matrix;
+
+
+/* prototypes */
+int parseMeans(void);
+
+#endif	/* MEANS_H */
